e41d20b898d56249c85aad8b0dcc9b218bb23ef6
Sahil Nagar <sahilnagr5786@gmail.com>
2023-01-18 14:51:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter, Routes,Route } from "react-router-dom"; 
+import Header from './Components/Header';
+import Home from './Components/Home';
+import Coins from './Components/Coins';
+import Exchanges from './Components/Exchanges';
+import CoinDetails from './Components/CoinDetails';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <HashRouter>
+  <Header/>
+   <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="/coins" element={<Coins/>} />
+    <Route path="/Exchanges" element={<Exchanges/>} />
+    <Route path="/coin/:id" element={<CoinDetails/>} />
+</Routes>
+<Footer/>
+ </HashRouter>
   );
 }
 
---
+++ src/ColorModeSwitcher.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+ const ColorModeSwitcher = props => {
+  const { toggleColorMode } = useColorMode();
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <IconButton
+    zIndex={"overlay"}
+      variant="ghost"
+      color="current"
+      pos={"fixed"}
+      top={"4"}
+      right={"4"}
+      marginLeft="2"
+      onClick={toggleColorMode}
+      icon={<SwitchIcon />}
+      {...props}
+    />
+  );
+};
+export default ColorModeSwitcher;
